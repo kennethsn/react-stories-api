b5c66765cc2c55c6eb352b0e29b575618d5fcc69
Kenneth Seals-Nutt <k.sealsnutt@gmail.com>
2019-11-18 10:13:18 -0800

    CONFIG: Exporting example component

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-stories-api'
+import { ExampleComponent } from 'react-stories-api';
 
 export default class App extends Component {
   render () {
