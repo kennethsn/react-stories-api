1ae2a1d1620899fc5763b21df3c10dbe7685a845
Kenneth Seals-Nutt <k.sealsnutt@gmail.com>
2019-11-23 09:54:59 -0800

    (#42) FIX: Updating react to use modern es style in example app

--- src/Components/Moment/index.js
+++ src/Components/Moment/index.js
@@ -1,7 +1,6 @@
 export MomentBase from './Base';
 export IFrameMoment from './IFrame';
-// SlideScroll is breaking the Example build, commenting out until fixed
-// export SlideScrollMoment from './SlideScroll';
+export SlideScrollMoment from './SlideScroll';
 export VideoMoment from './Video';
 export WikidataMoment from './Wikidata';
 export WikipediaMoment from './Wikipedia';
