90004e08460975a03e5f0c0998295c0da4a88a5f
Kenneth Seals-Nutt <k.sealsnutt@gmail.com>
2021-12-11 11:32:48 -0500

    (v0.0.1) FIX: Clean up Mirador integration

--- core/src/Components/Mirador/index.tsx
+++ core/src/Components/Mirador/index.tsx
@@ -1,5 +1,5 @@
 // Adapted from https://github.com/ProjectMirador/mirador/blob/master/src/lib/MiradorViewer.js
-import React, { CSSProperties, useState } from 'react';
+import React, { CSSProperties } from 'react';
 import {
   createGenerateClassName,
   createTheme,
@@ -7,18 +7,10 @@ import {
   StylesProvider,
 } from '@material-ui/core/styles';
 import deepmerge from 'deepmerge';
-import PluginProvider from 'mirador/dist/es/src/extend/PluginProvider';
-import App from 'mirador/dist/es/src/components/App';
-import createStore from 'mirador/dist/es/src/state/createStore';
-import createRootReducer from 'mirador/dist/es/src/state/reducers/rootReducer';
-import * as actions from 'mirador/dist/es/src/state/actions';
+// @ts-ignore
+import MiradorViewer from 'mirador/dist/es/src/lib/MiradorViewer';
 import settings from 'mirador/dist/es/src/config/settings';
-import {
-  getCompanionWindowIdsForPosition, getManifestSearchService,
-} from 'mirador/dist/es/src/state/selectors';
 import PropTypes from 'prop-types';
-import { Provider } from 'react-redux';
-import { v4 as uuid } from 'uuid';
 
 import './style.scss';
 
@@ -30,6 +22,7 @@ interface MiradorWindow {
 }
 
 export interface MiradorConfig {
+  id?: string;
   manifests?: Record<string, object>;
   thumbnailNavigation?: { defaultPosition: object };
   windows?: MiradorWindow[];
@@ -56,75 +49,15 @@ const Mirador = ({
   plugins,
   style,
 }: Props) => {
-  const [store] = useState(createStore());
-
-  /**
-   * Process config into actions
-   */
-  const processConfig = () => {
-    const mergedConfig = deepmerge(settings, config);
-    const action = actions.setConfig(mergedConfig);
-    store.dispatch(action);
-
-    mergedConfig.windows.forEach((miradorWindow, layoutOrder) => {
-      const windowId = `window-${uuid()}`;
-      const manifestId = miradorWindow.manifestId || miradorWindow.loadedManifest;
-      const manifestAction = store.dispatch(actions.fetchManifest(manifestId));
-      const windowAction = store.dispatch(actions.addWindow({
-        // these are default values ...
-        id: windowId,
-        layoutOrder,
-        manifestId,
-        thumbnailNavigationPosition: mergedConfig.thumbnailNavigation.defaultPosition,
-        // ... overridden by values from the window configuration ...
-        ...miradorWindow,
-      }));
-
-      Promise.all([manifestAction, windowAction]).then(() => {
-        if (miradorWindow.defaultSearchQuery) {
-          const state = store.getState();
-          const companionWindowId = getCompanionWindowIdsForPosition(
-            state,
-            { position: 'left', windowId },
-          )[0];
-          const searchService = getManifestSearchService(state, { windowId });
-          const searchId = searchService && `${searchService.id}?q=${miradorWindow.defaultSearchQuery}`;
-          if (companionWindowId && searchId) {
-            store.dispatch(
-              actions.fetchSearch(
-                windowId,
-                companionWindowId,
-                searchId,
-                miradorWindow.defaultSearchQuery,
-              ),
-            );
-          }
-        }
-      });
-    });
-
-    Object.keys(mergedConfig.manifests || {}).forEach((manifestId) => {
-      store.dispatch(
-        actions.requestManifest(manifestId, mergedConfig.manifests[manifestId]),
-      );
-    });
-  };
-  processConfig();
+  const mergedConfig = deepmerge(settings, config);
   return (
     <StylesProvider generateClassName={generateClassName}>
       <MuiThemeProvider theme={createTheme()}>
         <div
-          className={`story-mirador ${className}`}
           style={style}
+          className={`story-mirador ${className} mirador--initialized`}
         >
-          <Provider store={store}>
-            <PluginProvider
-              createRootReducer={createRootReducer}
-              plugins={plugins}
-            >
-              <App />
-            </PluginProvider>
-          </Provider>
+          { new MiradorViewer(mergedConfig, { plugins }).render() }
         </div>
       </MuiThemeProvider>
     </StylesProvider>
